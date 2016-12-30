481d372cdc551aabf3c2fff9204401cafe19bb53
Tyler Echols <tyechols@live.com>
2016-12-30 14:06:52 -0700

    Added .min condensed version. Use .min for the actual bookmarklet, with javascript: at the beginning

--- KanIt.js
+++ KanIt.js
@@ -1,3 +1,17 @@
 /**
  * Created by Tyler on 12/30/2016.
  */
+javascript:(function(){
+    // Respiratory system
+    $("#chkLungSound").attr('checked', true);           // Check box or radio button = checked
+    $("#drpRightUpperLungSound").val("1");              // Select option 1
+    $("#drpRightLowerLungSound").val("1");
+
+    // Genitourinary system
+    $("#rdbYellow").attr('checked', true);
+
+    // Musculoskeletal system
+    $("#chkAssistiveDevice").attr('checked', true);
+    $("#txtAssistiveDevice").val("Walker");             // Set text field
+})();
+
