5ad0016c3412c4fcf2970ba4cb30891b20036581
Tyler Echols <tyechols@live.com>
2016-12-31 12:15:10 -0700

    Created Supervisory form version of KanIt

---
+++ KanItSupervisory.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Tyler on 12/31/2016.
+ */
+javascript:(function(){
+    // Staff member info
+    $("#ddl_staff").val("4021");                                // Select option 4021 for "Hh, Aide" as of 12/31/2016
+    $("#rdblst_StaffPerson_1").attr('checked', true);           // Check box or radio button = checked
+    $("#chk_chha").attr('checked', true);
+
+    // Item 1
+    $("#rdblst_ques1_0").attr('checked', true);
+    $("#rdblst_ques2_0").attr('checked', true);
+    $("#rdblst_ques4_0").attr('checked', true);
+    $("#rdblst_ques7_0").attr('checked', true);
+    $("#rdblst_ques8_0").attr('checked', true);
+    $("#rdblst_ques9_0").attr('checked', true);
+    $("#rdblst_ques10_2").attr('checked', true);
+    $("#rdblst_ques11_0").attr('checked', true);
+    $("#rdblst_ques12_0").attr('checked', true);
+    $("#rdblst_ques13_0").attr('checked', true);
+
+    // Item 11
+    $("#rdblst_ques14_0").attr('checked', true);
+    $("#rdblst_ques15_0").attr('checked', true);
+    $("#rdblst_ques16_0").attr('checked', true);
+    $("#rdblst_ques17_0").attr('checked', true);
+    $("#rdblst_ques15_new_2").attr('checked', true);
+    $("#rdblst_ques16_new_0").attr('checked', true);
+    $("#rdblst_ques17_new_0").attr('checked', true);
+    $("#rdblst_ques18_0").attr('checked', true);
+    $("#rdblst_ques19_2").attr('checked', true);
+    $("#rdblst_ques20_2").attr('checked', true);
+    $("#rdblst_ques21_2").attr('checked', true);
+    $("#rdblst_ques22_2").attr('checked', true);
+
+    // Signature
+    $("#Chk_Signature").attr('checked', true);
+
+})();
